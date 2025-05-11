5f49c5199810e2c68f97f4edb1533ed1de210d3b
Даниил Давлетов <d.daniel.d@list.ru>
2025-05-11 19:37:32 +0500

    Print in order

---
+++ Problems/Concurrency/PrintInOrder.cs
@@ -0,0 +1,37 @@
+﻿namespace Problems.Concurrency;
+
+public class PrintInOrder
+{
+}
+
+public class Foo {
+
+    public Foo() {
+        
+    }
+
+    private AutoResetEvent _firstPrintedEvent = new AutoResetEvent(false);
+    private AutoResetEvent _secondPrintedEvent = new AutoResetEvent(false);
+    
+    public void First(Action printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        _firstPrintedEvent.Set();
+    }
+
+    public void Second(Action printSecond) {
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        _firstPrintedEvent.WaitOne();
+        printSecond();
+        _secondPrintedEvent.Set();
+    }
+
+    public void Third(Action printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        _secondPrintedEvent.WaitOne();
+        printThird();
+    }
+}
