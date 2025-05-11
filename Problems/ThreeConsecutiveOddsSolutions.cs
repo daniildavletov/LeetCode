e0b9c73438a440226ef087af548fcf7ffc9fed9f
Даниил Давлетов <d.daniel.d@list.ru>
2025-05-11 19:51:25 +0500

    ThreeConsecutiveOdds

---
+++ Problems/ThreeConsecutiveOddsSolutions.cs
@@ -0,0 +1,27 @@
+﻿namespace Problems;
+
+public class ThreeConsecutiveOddsSolutions
+{
+    public bool ThreeConsecutiveOdds(int[] arr)
+    {
+        int счетчикНечетныхЧисел = 0;
+
+        foreach (var odd in arr)
+        {
+            if (odd % 2 == 0)
+            {
+                счетчикНечетныхЧисел = 0;
+                continue;
+            }
+
+            счетчикНечетныхЧисел++;
+
+            if (счетчикНечетныхЧисел == 3)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
