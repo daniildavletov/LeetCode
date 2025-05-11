b11aabec71c92c695c84e9a68ede0b613d3c70d5
Даниил Давлетов <d.daniel.d@list.ru>
2025-05-11 19:04:58 +0500

    ValidParentheses

--- Problems/ValidParentheses.cs
+++ Problems/ValidParentheses.cs
@@ -2,4 +2,35 @@
 
 public class ValidParentheses
 {
+    private readonly Dictionary<char, char> _matches = new()
+    {
+        { '(', ')' },
+        { '{', '}' },
+        { '[', ']' },
+    };
+    
+    public bool IsValid(string s)
+    {
+        var bracketsArray = s.ToCharArray();
+        var openBracketsStack = new Stack<char>();
+        foreach (var bracket in bracketsArray)
+        {
+            if (_matches.ContainsKey(bracket))
+            {
+                openBracketsStack.Push(bracket);
+                continue;
+            }
+
+            if (openBracketsStack.Count == 0)
+            {
+                return false;
+            }
+            
+            if (_matches[openBracketsStack.Peek()] != bracket) return false;
+            
+            openBracketsStack.Pop();
+        }
+
+        return openBracketsStack.Count == 0;
+    }
 }
