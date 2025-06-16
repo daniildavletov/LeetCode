f380d41de4d1b28f8a3e6621a00801469999ff55
Даниил Давлетов <d.daniel.d@list.ru>
2025-06-16 21:03:35 +0500

    FindPeakElementSolution

---
+++ Problems/FindPeakElementSolution.cs
@@ -0,0 +1,48 @@
+﻿namespace Problems;
+
+public class FindPeakElementSolution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        int midIndex = nums.Length / 2;
+        
+        int peak = 0;
+
+        while (peak == 0)
+        {
+            if (midIndex == 0)
+            {
+                peak = nums[midIndex];
+                continue;
+            }
+            
+            if (midIndex == nums.Length - 1)
+            {
+                if (nums[midIndex] > nums[midIndex-1])
+                {
+                    peak = nums[midIndex];   
+                }
+                else
+                {
+                    return midIndex - 1;
+                }
+                continue;
+            }
+            
+            if (nums[midIndex] > nums[midIndex-1])
+            {
+                if (nums[midIndex] < nums[midIndex+1])
+                {
+                    midIndex++;
+                    continue;
+                }
+
+                peak = nums[midIndex];
+                continue;
+            }
+            midIndex--;
+        }
+        
+        return midIndex;
+    }
+}
---
+++ Tests/FindPeakElementSolutionTests.cs
@@ -0,0 +1,54 @@
+﻿using Problems;
+
+namespace Tests;
+
+public class FindPeakElementSolutionTests
+{
+    private FindPeakElementSolution _findPeakElementSolution;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _findPeakElementSolution = new FindPeakElementSolution();
+    }
+
+    [Test]
+    public void FindPeakElement_TwoPeaks_FindLast()
+    {
+        int[] nums = [1, 2, 1, 3, 5, 6, 4];
+
+        var peakIndex = _findPeakElementSolution.FindPeakElement(nums);
+        
+        Assert.That(peakIndex, Is.EqualTo(5));
+    }
+    
+    [Test]
+    public void FindPeakElement_OneElement_FindZero()
+    {
+        int[] nums = [1];
+
+        var peakIndex = _findPeakElementSolution.FindPeakElement(nums);
+        
+        Assert.That(peakIndex, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void FindPeakElement_DecreaseArray_FindLeftPeak()
+    {
+        int[] nums = [2, 1];
+
+        var peakIndex = _findPeakElementSolution.FindPeakElement(nums);
+        
+        Assert.That(peakIndex, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void FindPeakElement_IncreaseArray_FindRightPeak()
+    {
+        int[] nums = [1, 2];
+
+        var peakIndex = _findPeakElementSolution.FindPeakElement(nums);
+        
+        Assert.That(peakIndex, Is.EqualTo(1));
+    }
+}
